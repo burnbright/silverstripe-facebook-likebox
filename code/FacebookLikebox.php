6443fd1392a6d1ca83910d36b6c4da4b1e5f7324
helpfulrobot <chris@silverstripe.com>
2016-01-01 05:43:27 +1300

    Converted to PSR-2

--- code/FacebookLikebox.php
+++ code/FacebookLikebox.php
@@ -1,64 +1,70 @@
 <?php
 
-class FacebookLikebox extends ViewableData{
+class FacebookLikebox extends ViewableData
+{
 
-	protected $parameters = array(
-		'Href' => 'http://www.facebook.com/platform',
-		'Width' => 300,
-		'Height' => 60,
-		'AdaptContainerWidth' => true,
-		'ShowPagePosts' => false,
-		'ShowFaces' => false, 
-		'HideCoverPhoto' => true, 
-		'UseSmallHeader' => true
-	);
+    protected $parameters = array(
+        'Href' => 'http://www.facebook.com/platform',
+        'Width' => 300,
+        'Height' => 60,
+        'AdaptContainerWidth' => true,
+        'ShowPagePosts' => false,
+        'ShowFaces' => false,
+        'HideCoverPhoto' => true,
+        'UseSmallHeader' => true
+    );
 
-	function __construct($parameters = null){
-		if($parameters){
-			$this->setParameters($parameters);
-		}
-	}
+    public function __construct($parameters = null)
+    {
+        if ($parameters) {
+            $this->setParameters($parameters);
+        }
+    }
 
-	function setParameters($parameters){
-		$this->parameters = array_merge($this->parameters, $parameters);
-	}
-	
-	public function getAttributes(){
-		$params = $this->parameters;
+    public function setParameters($parameters)
+    {
+        $this->parameters = array_merge($this->parameters, $parameters);
+    }
+    
+    public function getAttributes()
+    {
+        $params = $this->parameters;
 
-		$attrs = array(
-			'data-href' => $params['Href']
-		);
+        $attrs = array(
+            'data-href' => $params['Href']
+        );
 
-		$attrs['data-width'] = $params['Width'] ? $params['Width'] : '';
-		$attrs['data-height'] = $params['Height'] ? $params['Height'] : '';
-		$attrs['data-adapt-container-width'] = $params['AdaptContainerWidth'] ? 'true' : 'false';
-		$attrs['data-small-header'] = $params['UseSmallHeader'] ? 'true' : 'false';
-		$attrs['data-hide-cover'] = $params['HideCoverPhoto'] ? 'true' : 'false';
-		$attrs['data-show-facepile'] = $params['ShowFaces'] ? 'true' : 'false';
-		$attrs['data-show-posts'] = $params['ShowPagePosts'] ? 'true' : 'false';
-		
-		return $attrs;
-	}	
+        $attrs['data-width'] = $params['Width'] ? $params['Width'] : '';
+        $attrs['data-height'] = $params['Height'] ? $params['Height'] : '';
+        $attrs['data-adapt-container-width'] = $params['AdaptContainerWidth'] ? 'true' : 'false';
+        $attrs['data-small-header'] = $params['UseSmallHeader'] ? 'true' : 'false';
+        $attrs['data-hide-cover'] = $params['HideCoverPhoto'] ? 'true' : 'false';
+        $attrs['data-show-facepile'] = $params['ShowFaces'] ? 'true' : 'false';
+        $attrs['data-show-posts'] = $params['ShowPagePosts'] ? 'true' : 'false';
+        
+        return $attrs;
+    }
 
-	public function getAttributesHTML(){
-		$attrs = $this->getAttributes();
+    public function getAttributesHTML()
+    {
+        $attrs = $this->getAttributes();
 
-		// Remove empty
-		$attrs = array_filter((array)$attrs, function($v) {
-			return ($v || $v === 0 || $v === '0');
-		}); 
+        // Remove empty
+        $attrs = array_filter((array)$attrs, function ($v) {
+            return ($v || $v === 0 || $v === '0');
+        });
 
-		// Create markkup
-		$parts = array();
-		foreach($attrs as $name => $value) {
-			$parts[] = "{$name}=\"" . Convert::raw2att($value) . "\"";
-		}
+        // Create markkup
+        $parts = array();
+        foreach ($attrs as $name => $value) {
+            $parts[] = "{$name}=\"" . Convert::raw2att($value) . "\"";
+        }
 
-		return implode(' ', $parts);
-	}
+        return implode(' ', $parts);
+    }
 
-	public function forTemplate(){
-		return $this->renderWith('FacebookLikebox');
-	}
+    public function forTemplate()
+    {
+        return $this->renderWith('FacebookLikebox');
+    }
 }
--- code/FacebookLikeboxWidget.php
+++ code/FacebookLikeboxWidget.php
@@ -1,66 +1,67 @@
 <?php
 
-if(class_exists("Widget")){
+if (class_exists("Widget")) {
+    class FacebookLikeboxWidget extends Widget
+    {
 
-	class FacebookLikeboxWidget extends Widget{
+        private static $cmstitle = "Facebook Likebox";
+        
+        private static $db = array(
+            'Href' => 'Text',
+            'Width' => 'Int',
+            'Height' => 'Int',
+            'AdaptContainerWidth' => 'Boolean',
+            'ShowPagePosts' => 'Boolean',
+            'ShowFaces' => 'Boolean',
+            'HideCoverPhoto' => 'Boolean',
+            'UseSmallHeader' => 'Boolean'
+        );
 
-		private static $cmstitle = "Facebook Likebox";
-		
-		private static $db = array(
-			'Href' => 'Text',
-			'Width' => 'Int',
-			'Height' => 'Int',
-			'AdaptContainerWidth' => 'Boolean',
-			'ShowPagePosts' => 'Boolean',
-			'ShowFaces' => 'Boolean', 
-			'HideCoverPhoto' => 'Boolean', 
-			'UseSmallHeader' => 'Boolean'
-		);
+        private static $defaults = array(
+            'ShowFaces' => false,
+            'Header' => false
+        );
 
-		private static $defaults = array(
-			'ShowFaces' => false,
-			'Header' => false
-		);
+        public function getCMSFields()
+        {
+            $fields = parent::getCMSFields();
+            $fields->merge(new FieldList(
+                TextField::create("Href", "Facebook Page URL")
+                    ->setRightTitle("eg: http://www.facebook.com/mypage"),
+                CheckboxField::create('ShowPagePosts')
+                    ->setDescription("i.e. the latest posts and images"),
+                CheckboxField::create('ShowFaces'),
+                CheckboxField::create('HideCoverPhoto'),
+                CheckboxField::create('UseSmallHeader'),
+                NumericField::create('Width'),
+                NumericField::create('Height'),
+                CheckboxField::create('AdaptContainerWidth')
+                    ->setDescription('Plugin will try to fit the full width of the container'),
+                LiteralField::create('HeightWarning',
+                    '<p class="message">note: some features will not be visible if the chosen height is too short.</p>'
+                )
+            ));
+            return $fields;
+        }
 
-		function getCMSFields(){
-			$fields = parent::getCMSFields();
-			$fields->merge(new FieldList(
-				TextField::create("Href","Facebook Page URL")
-					->setRightTitle("eg: http://www.facebook.com/mypage"),
-				CheckboxField::create('ShowPagePosts')
-					->setDescription("i.e. the latest posts and images"),
-				CheckboxField::create('ShowFaces'),
-				CheckboxField::create('HideCoverPhoto'),
-				CheckboxField::create('UseSmallHeader'),
-				NumericField::create('Width'),
-				NumericField::create('Height'),
-				CheckboxField::create('AdaptContainerWidth')
-					->setDescription('Plugin will try to fit the full width of the container'),
-				LiteralField::create('HeightWarning',
-					'<p class="message">note: some features will not be visible if the chosen height is too short.</p>'
-				)
-			));
-			return $fields;
-		}
+        public function getLikebox()
+        {
+            $lb = new FacebookLikebox();
+            $lb->setParameters(array(
+                'Href' => $this->Href,
+                'Width' => $this->Width,
+                'Height' => $this->Height,
+                'AdaptContainerWidth' => $this->AdaptContainerWidth,
+                'ShowPagePosts' => $this->ShowPagePosts,
+                'ShowFaces' => $this->ShowFaces,
+                'HideCoverPhoto' => $this->HideCoverPhoto,
+                'UseSmallHeader' => $this->UseSmallHeader
+            ));
+            return $lb;
+        }
 
-		function getLikebox(){
-			$lb = new FacebookLikebox();
-			$lb->setParameters(array(
-				'Href' => $this->Href,
-				'Width' => $this->Width,
-				'Height' => $this->Height,
-				'AdaptContainerWidth' => $this->AdaptContainerWidth,
-				'ShowPagePosts' => $this->ShowPagePosts,
-				'ShowFaces' => $this->ShowFaces, 
-				'HideCoverPhoto' => $this->HideCoverPhoto, 
-				'UseSmallHeader' => $this->UseSmallHeader
-			));
-			return $lb;
-		}
-
-		function getTitle(){
-
-		}
-	}
-
+        public function getTitle()
+        {
+        }
+    }
 }
